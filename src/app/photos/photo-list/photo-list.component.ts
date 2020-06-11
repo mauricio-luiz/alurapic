e74148efb3d8ccbe492ef1697bb36653050c0061
mauricio <mauriciojunio@gmail.com>
2020-06-10 22:22:22 -0300

    :art: parametrizado a rota do servico das fotos de usuario

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 
@@ -11,11 +13,15 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService){}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ){}
   
   ngOnInit(): void{
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe(photos => {          
         this.photos = photos
       });

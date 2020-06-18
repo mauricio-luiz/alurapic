fb4d0edd0e3697b07842bba045782d07b527d56b
mauricio <mauriciojunio@gmail.com>
2020-06-18 20:15:32 -0300

    :rocket: adicionado servico para detectar se o funcionamento do codigo é no navegador ou server side

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/core/auth.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlataformDetectorService } from 'src/app/core/plataform-detector/plataform-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -14,7 +15,8 @@ export class SignInComponent implements OnInit{
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
-        private router: Router) { }
+        private router: Router,
+        private platformDetectorService: PlataformDetectorService) { }
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -34,7 +36,8 @@ export class SignInComponent implements OnInit{
                 err => { 
                     console.log(err);
                     this.loginForm.reset();
-                    this.userNameInput.nativeElement.focus(); //Nao pode usar o Renderer
+                    this.platformDetectorService.isPlatformBrowser() && 
+                        this.userNameInput.nativeElement.focus(); //Nao pode usar o Renderer
                     alert('Invalid username or password');
                 }
             )

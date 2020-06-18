6115a3872b76bbc5e9815bcfb18769cc961ab691
mauricio <mauriciojunio@gmail.com>
2020-06-18 19:18:39 -0300

    :rocket: adicionado pagina de login com validacao required

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,10 +5,16 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { SignInComponent } from './home/signin/signin.component';
 
 const routes: Routes = [
     { 
-        path: 'user/:userName', component: PhotoListComponent,
+        path: '',
+        component: SignInComponent
+    },
+    { 
+        path: 'user/:userName', 
+        component: PhotoListComponent,
         resolve: {
             photos: PhotoListResolver
         }
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SignInComponent } from './signin/signin.component';
+
+@NgModule({
+    declarations: [ SignInComponent ],
+    imports: [ 
+        CommonModule,
+        ReactiveFormsModule         
+    ]
+})
+export class HomeModule {}

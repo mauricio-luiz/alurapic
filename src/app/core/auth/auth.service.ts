373121238e2c71c25305eb466f6512d7430fe101
mauricio <mauriciojunio@gmail.com>
2020-07-02 18:49:07 -0300

    :rocket: adicionado servico para armazenar, remover, validar e retornar o token provido da api

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,7 +10,7 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenService : TokenService) { }
 
   authenticate(userName: string, password: string){
     return this.http
@@ -20,6 +21,7 @@ export class AuthService {
       )
       .pipe(tap( res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(`User ${userName} authenticated with token ${authToken}`);
       }));
   }

77fb2a2e0f6da6c8d7353baee87ad08179da2d1b
mauricio <mauriciojunio@gmail.com>
2020-08-17 20:37:57 -0300

    :rocket: adicionado validacao custumizada

---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { SignupService } from './signup.service';
+import { debounceTime, map, switchMap, first } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root'})
+export class UserNotTakenValidatorService{
+
+    constructor(private signUpService: SignupService){}
+
+    checkUserNameTaken(){
+        return (control: AbstractControl) => {
+            return control
+                .valueChanges
+                .pipe(debounceTime(300))
+                .pipe(switchMap(userName => 
+                    this.signUpService.checkUserNameTaken(userName)
+                ))
+                .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))
+                .pipe(first());
+        }
+    }
+}

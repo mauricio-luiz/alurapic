8bd32a45616281e7518385afeb607f08f1b91aa7
mauricio <mauriciojunio@gmail.com>
2020-08-17 19:15:26 -0300

    :construction: iniciado modulo de signup

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignUpComponent } from './signup/signup.componet';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [ 
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
-        RouterModule    
+        RouterModule
     ]
 })
 export class HomeModule {}

070c5942b05f5f0c3454d0c89b57c31411e39094
mauricio <mauriciojunio@gmail.com>
2020-09-28 12:13:45 -0300

    :rocket: upload de imagem

--- src/app/photos/photo-form/photo-form.mudule.ts
+++ src/app/photos/photo-form/photo-form.mudule.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
-    imports:[ CommonModule ]
+    imports:[ 
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule,
+        RouterModule
+    ]
 })
 export class PhotoFormModule {}
